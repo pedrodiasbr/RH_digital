2461efab9a0a4cc4c8f2793b161bfe3c6f478054
LucasLopes <lucas.lopes@meta3.com.br>
2020-02-28 19:53:42 -0300

    Update Program.cs
    Lucas. Cria: Classe empregado e DBContext . Enums StatusEmpregado e Sexo.

--- RH-digital/Program.cs
+++ RH-digital/Program.cs
@@ -8,8 +8,42 @@ namespace RH_digital
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+
         }
+    }
+
+    public class Empregado
+    {
+        public string Nome { set; get; }
+        public DateTime DataNascimento { set; get; }
+        public string CPF { set; get; }
+        public string Sexo { set; get; }
+        public string Nacionalidade { set; get; }
+        public decimal Salario { set; get; }
+        public string Cargo { set; get; }
+        public string Status { set; get; }
+        public int NumeroPessoal { set; get; }
+    }
+
+    public class DBContext
+    {
+
+    }
+
+    public enum StatusEmpregado
+    {
+        Ativo = 1,
+        Afastado = 2,
+        Aposentado = 3,
+        Desligado = 4
+    }
+
+    public enum Sexo
+    {
+        Masculino = 1,
+        Feminino = 2,
+        Outros = 3
     }
 }
